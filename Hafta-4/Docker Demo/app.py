0ef8861045c48a01428725a41c353e4a103ec2ce
Yunus Emre Gündoğmuş <yemregun@icloud.com>
2022-12-04 10:46:11 +0300

    Update app.py

--- Hafta-4/Docker Demo/app.py
+++ Hafta-4/Docker Demo/app.py
@@ -9,7 +9,6 @@ check_button = st.button("Duygusunu bul!")
 
 if check_button:
     result = api.sentiment_analysis(text)
-    if result["evaluation"]["label"] == 'Pozitif':
-        st.markdown(f'<h3>%{round(result["evaluation"]["score"]*100, 2)} olasılıkla </h3><h1 style="color:#33ff33;font-size:24px;">Pozitif</h1>', unsafe_allow_html=True)
-    else:
-        st.markdown(f'<h3>%{round(result["evaluation"]["score"]*100, 2)} olasılıkla </h3><h1 style="color:#FF0000;font-size:24px;">Negatif</h1>', unsafe_allow_html=True)
+    st.write(f'Sonuç: %{round(result["evaluation"]["score"]*100, 2)} olasılıkla {result["evaluation"]["label"]}')
+
+
